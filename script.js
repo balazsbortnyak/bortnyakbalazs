f6fc771e2b9e636af92c2a2eac85f506f31270fc
Willsee <78503694+imwillsee@users.noreply.github.com>
2022-02-15 20:07:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Webdesign", "Videó vágás", "Motion design", "Grafikai munkák"],
+        strings: ["Webdesign", "Videó vágás", "Motion design", "Grafikai munkák, Webfejlesztés"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Webdesign", "Videó vágás", "Motion design", "Grafikai munkák"],
+        strings: ["Webdesign", "Videó vágás", "Motion design", "Grafikai munkák, Webfejlesztés"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
